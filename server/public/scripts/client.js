4c5fa89b99f0a5cd1bd6d2814420a01b36e6bf1d
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2022-12-14 18:32:46 -0600

    added buttons in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,18 +1,43 @@
 $(document).ready(onReady);
 
 function onReady() {
-    getSongs();
+    getSongs();     // on page load, this is a GET request
     $('#add').on('click', postSong);
+    $('#songsTableBody').on('click', '.delete', handleDelete);
+    $('#songsTableBody').on('click', '.rank-up', handleRankUp);
+    $('#songsTableBody').on('click', '.rank-down', handleRankDown);
 }
+
+function handleRankUp() {
+    console.log('rank up');
+}   // end handleRankUp
+
+function handleRankDown() {
+    console.log('rank down');
+}   // end handleRankDown
+
+
+function handleDelete() {
+    const id = $(this).data('id');  // neatens up the code we use in url:
+    $.ajax({
+        type: 'DELETE',
+        url: `/musicLibrary/${id}`
+    }).then(function() {
+        getSongs(); // when you've successfully deleted the songs, get us the new songs list
+    }).catch(function(error) {
+        console.log('error with deleting', error);
+    })
+}   // end of handleDelete
+
 
 // get artist data from the server
 function getSongs() {
     $("#songsTableBody").empty();
     $.ajax({
         type: 'GET',
-        url: '/songs'
+        url: '/musicLibrary'
     }).then(function (response) {
-        console.log("GET /songs response", response);
+        console.log("GET /musicLibrary response", response);
         // append data to the DOM
         for (let i = 0; i < response.length; i++) {
             $('#songsTableBody').append(`
@@ -21,6 +46,15 @@ function getSongs() {
                     <td>${response[i].track}</td>
                     <td>${response[i].rank}</td>
                     <td>${response[i].published}</td>
+                    <td>
+                        <button class="rank-up">UP</button>
+                        <button class="rank-down">DOWN</button>
+                    </td>
+                    <td>
+                        <button class="delete" data-id= ${response[i].id}>
+                            DELETE
+                        </button>
+                    </td>
                 </tr>
             `);
         }
@@ -36,7 +70,7 @@ function postSong() {
     }
     $.ajax({
         type: 'POST',
-        url: '/songs',
+        url: '/musicLibrary',
         data: payloadObject
     }).then( function (response) {
         $('#artist').val(''),
--- server/routes/musicLibrary.js
+++ server/routes/musicLibrary.js
@@ -16,13 +16,48 @@ pool.query(queryText)   // do query and then
 })
 });
 
+// router.get('/:artist', (req, res) => {
+//     `SELECT * from songs WHERE artist = ${req.params.artist}`
+// });
+
+router.get('/:id', (req, res) => {
+    console.log("Hello from get request!", req.params.id);
+    const queryText = `SELECT * from songs WHERE id = ${req.params.id};`;
+    pool.query(queryText)   // do query and then
+.then((result) => {
+    console.log(result);
+    res.sendStatus(204);  // will make our results show up in rows
+})  // do callback error function
+.catch((error) => {
+    console.log('error making a query', error);
+    res.sendStatus(204);    // we didn't have this originally and when we ran it is just loaded forever
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    console.log("Hello from delete request!", req.params.id);
+    const queryText = `DELETE from songs WHERE id = ${req.params.id};`;
+    pool.query(queryText)   // do query and then
+.then((result) => {
+    console.log(result);
+    res.send(202);  // will make our results show up in rows
+})  // do callback error function
+.catch((error) => {
+    console.log('error making a query', error);
+    res.sendStatus(500);    // we didn't have this originally and when we ran it is just loaded forever
+    });
+});
+
+
+
 router.post('/', (req, res) => {    // send the data back w/ this function
     const newSong = req.body;   // req.body goes in the POST
     const queryText = `
-    INSERT INTO "songs" ("rank", "artist", "track", "published")    
-    VALUES (${newSong.rank}, '${newSong.artist}', '${newSong.track}',' ${newSong.published}');    
+    INSERT INTO  "songs" ("rank", "artist", "track", "published")    
+    VALUES ($1, $2, $3, $4);    
     `;   // inserting data into the database    (we hardcoded first, then when that worked we used string interpolation)
-    pool.query(queryText)
+    // ${newSong.rank}, '${newSong.artist}', '${newSong.track}',' ${newSong.published}'
+    pool.query(queryText, [newSong.rank, newSong.artist, newSong.track, newSong.published])
     .then((result) => {
         console.log('result', result);
         res.sendStatus(201);
